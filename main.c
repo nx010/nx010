80a5ac48b85c7340f7e2b2ba68dc63da51f8d4ed
Antonio Martinez <antoniomartinez0791@gmail.com>
2021-10-17 21:23:35 +0200

    insertar mismo sitiinsertar mismo sitioo

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include "incluce/funciones.h"
 int main(int argc, char** argv) {
 	printf("Hola, mundo.\n);
-	printf("dos y dos son \d.\n", sumar (2,2));
+	printf(" 2 + 2= \d\n", sumar (2,2));
+	printf(" 2 + 2= \d\n", sumar (2,2));
+	printf(" 2 + 2= \d\n", sumar (2,2));
+	printf(" 2 + 2= \d\n", sumar (2,2));
 	return 0;
+
 }
